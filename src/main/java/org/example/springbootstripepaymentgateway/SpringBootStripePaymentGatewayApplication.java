546f3bdddc2f1b0a4863995f305bd75685dc442c
shivakant singh <shivakant5875@gmail.com>
2024-12-02 19:55:42 +0530

    nothing to commit

---
+++ src/main/java/org/example/springbootstripepaymentgateway/SpringBootStripePaymentGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootstripepaymentgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStripePaymentGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStripePaymentGatewayApplication.class, args);
+    }
+
+}
